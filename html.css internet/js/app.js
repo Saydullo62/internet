81144f5a5b2c8adf1cb8a46f64717665d5c1c01f
Saydullo62 <101372997+Saydullo62@users.noreply.github.com>
2022-03-22 04:06:54 +0300

    Add files via upload

---
+++ html.css internet/js/app.js
@@ -0,0 +1,22 @@
+const checkStatus = document.querySelector(".checkStatus");
+
+let online = () => {
+    checkStatus.innerText = "You are ON-LINE😎";
+    checkStatus.style.backgroundColor = "green";
+};
+let offline = () => {
+    checkStatus.innerText = "You are OFF-LINE😟";
+    checkStatus.style.backgroundColor = "red";
+};
+
+if (window.navigator.onLine) {
+    online();
+}
+else {
+    offline();
+}
+
+
+window.addEventListener("online", online);
+window.addEventListener("offline", offline);
+
